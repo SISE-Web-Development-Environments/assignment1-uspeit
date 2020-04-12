f86ac2128c1f8bbaff72c88c37765961b1b1e983
Eitan Fedenko <eitan.lp@gmail.com>
2020-04-12 14:07:26 +0300

    Finishes

--- js/gallery.js
+++ js/gallery.js
@@ -1,27 +1,24 @@
-this.photoUrls = ['https://i.redd.it/lntb7cl5uzs31.jpg', 
-'https://i.redd.it/0pkj1x4r1bl41.jpg',
-'https://i.redd.it/r5nxyz10v2p41.jpg',
- 'https://i.redd.it/eodk675sytm41.jpg',
- 'https://i.redd.it/gy3k46k309o41.jpg', 
- 'https://i.redd.it/sx0e6jebqsq41.jpg',
- 'https://i.redd.it/c1dmgplmbhp41.jpg', 
- 'https://i.redd.it/2xmylvhhvmp41.jpg',
- 'https://i.redd.it/j81yc8lig4m41.jpg', 
- 'https://i.redd.it/dt94tz18csp41.jpg'];
+const photoIds = ['lntb7cl5uzs31', '0pkj1x4r1bl41', 'r5nxyz10v2p41', 'eodk675sytm41', 'gy3k46k309o41', 'sx0e6jebqsq41', 'c1dmgplmbhp41', '2xmylvhhvmp41', 'j81yc8lig4m41', 'dt94tz18csp41'];
+const photoUrls = photoIds.map(id => `https://i.redd.it/${id}.jpg`);
 const container = $('#gallery');
 var currentPage;
 
 function addPhoto(url) {
-    if (currentPage === undefined || currentPage.children().length >= 10) { 
+    if (currentPage === undefined || currentPage.children().length >= 10) {
         currentPage = $('<ul></ul>');
         container.append(currentPage);
     }
     currentItem = $('<li></li>');
 
+    let linkElement = $('<a></a>');
+    linkElement.attr('href', url)
+    linkElement.attr('target', '_blank')
+    
     let imageElement = $('<img/>');
     imageElement.attr('src', url);
-    
-    currentItem.append(imageElement);
+
+    linkElement.append(imageElement);
+    currentItem.append(linkElement);
     currentPage.append(currentItem);
 }
 
